0e15f2a5692ef8f5e842b5b5c8d489d12657c46a
tarun <tarun.ranka@groupfmg.com>
2015-10-12 18:39:49 +0530

    Second version

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var postcss = require('gulp-postcss');
+var gulp = require('gulp');
+var autoprefixer = require('autoprefixer');
+var mqpacker = require('css-mqpacker');
+var csswring = require('csswring');
+
+gulp.task('css', function () {
+    var processors = [
+        autoprefixer({browsers: ['last 1 version']}),
+        mqpacker,
+        csswring
+    ];
+    return gulp.src('./css/*.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./dest'));
+});
